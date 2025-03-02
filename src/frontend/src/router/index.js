147930c87e3888b231b30c9499b1877216967457
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-03-02 15:20:02 +0200

    added new routes,home and page not found

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -6,10 +6,20 @@ import LocationsView from "@/components/LocationsView.vue";
 import LocationDetailView from "@/components/LocationDetailView.vue";
 import EpisodesView from "@/components/EpisodesView.vue";
 import EpisodeDetailsView from "@/components/EpisodeDetailsView.vue";
+import HomeView from "@/components/HomeView.vue";
+import PageNotFound from "@/components/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: {
+        title: "Rick and Morty",
+      },
+    },
     // Characters
     {
       path: "/characters",
@@ -65,9 +75,17 @@ const router = createRouter({
       },
     },
 
+    // Page not found
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: PageNotFound,
+      meta: {
+        title: "Page not found!",
+      },
+    },
   ],
 });
-
 
 // Update document title based on route meta
 router.beforeEach((to, from, next) => {
