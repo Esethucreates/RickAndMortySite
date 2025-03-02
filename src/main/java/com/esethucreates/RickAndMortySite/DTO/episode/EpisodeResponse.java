785e724bf61589fe7f82e5665ee249d59cd3ab47
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-03-02 15:19:16 +0200

    modified name

--- src/main/java/com/esethucreates/RickAndMortySite/DTO/episode/EpisodeResponse.java
+++ src/main/java/com/esethucreates/RickAndMortySite/DTO/episode/EpisodeResponse.java
@@ -3,7 +3,7 @@ package com.esethucreates.RickAndMortySite.DTO.episode;
 import java.util.List;
 
 public record EpisodeResponse(
-	String airDate,
+	String air_date,
 	List<String> characters,
 	String name,
 	String episode,
