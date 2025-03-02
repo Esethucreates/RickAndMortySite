7312f6bb7183bf6cd70cb4f2011bac2855cfc9d7
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-03-02 15:20:50 +0200

    Added new PrimeVue package

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,8 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// PrimeVue UI
+import PrimeVue from "primevue/config";
+import Lara from "@primeuix/themes/lara";
+
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+    options: {
+      darkModeSelector: false || "none",
+    },
+  },
+});
 
 app.mount("#app");
